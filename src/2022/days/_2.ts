5ead16e9466fc49d14fd7fc33bc2e97b8c11f5de
1910653094 <fabian.peyrat01@gmail.com>
2022-12-02 20:11:19 +0100

    solve day 2 and add eslint

---
+++ src/2022/days/_2.ts
@@ -0,0 +1,102 @@
+import path from 'path';
+import { reader } from '../../outils';
+
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+const loss = 0;
+const draw = 3;
+const win = 6;
+
+enum Moves {
+  opponentRock = 'A',
+  opponentPaper = 'B',
+  opponentScissors = 'C',
+  myRock = 'X',
+  myPaper = 'Y',
+  myScissors = 'Z',
+}
+
+const getPoints = (move: string): number => {
+  if (move === Moves.myRock || move === Moves.opponentRock) return rock;
+  if (move === Moves.myPaper || move === Moves.opponentPaper) return paper;
+  return scissors;
+};
+
+const getMove = (opponent: string, needAWin: boolean): Moves => {
+  if (needAWin) {
+    if (opponent === Moves.opponentRock) return Moves.myPaper;
+
+    if (opponent === Moves.opponentScissors) return Moves.myRock;
+
+    return Moves.myScissors;
+  }
+
+  if (opponent === Moves.opponentRock) return Moves.myScissors;
+
+  if (opponent === Moves.opponentPaper) return Moves.myRock;
+
+  return Moves.myPaper;
+};
+
+export const main = () => {
+  const file = reader(path.join(__dirname, '../input/_2.txt'));
+  const moves = file.split('\n');
+
+  const points = moves.reduce((sum: number, move: string) => {
+    const [opponent, me] = move.split(' ');
+
+    // win
+    if (
+      (opponent === Moves.opponentRock && me === Moves.myPaper) ||
+      (opponent === Moves.opponentPaper && me === Moves.myScissors) ||
+      (opponent === Moves.opponentScissors && me === Moves.myRock)
+    ) {
+      return sum + win + getPoints(me);
+    }
+
+    // loss
+    if (
+      (opponent === Moves.opponentRock && me === Moves.myScissors) ||
+      (opponent === Moves.opponentPaper && me === Moves.myRock) ||
+      (opponent === Moves.opponentScissors && me === Moves.myPaper)
+    ) {
+      return sum + loss + getPoints(me);
+    }
+
+    // draw
+    return sum + draw + getPoints(me);
+  }, 0);
+
+  console.log(points);
+};
+
+const task2 = () => {
+  const lossMove = 'X';
+  const winMove = 'Z';
+
+  const file = reader(path.join(__dirname, '../input/_2.txt'));
+  const moves = file.split('\n');
+
+  const points = moves.reduce((sum: number, move: string) => {
+    const [opponent, outcome] = move.split(' ');
+
+    // loss
+    if (outcome === lossMove) {
+      return sum + loss + getPoints(getMove(opponent, false));
+    }
+
+    // win
+    if (outcome === winMove) {
+      return sum + win + getPoints(getMove(opponent, true));
+    }
+
+    // draw
+    return sum + draw + getPoints(opponent);
+  }, 0);
+
+  console.log(points);
+};
+
+main();
+task2();
--- src/outils/fileWorker.ts
+++ src/outils/fileWorker.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 
 export const reader = (filePath: string): string => {
-    const file = readFileSync(filePath, 'utf-8');
+  const file = readFileSync(filePath, 'utf-8');
 
-    return file;
-}
+  return file;
+};
