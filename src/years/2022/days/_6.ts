31f14f897e4e197f29624ecc921312b58b49e8cd
1910653094 <fabian.peyrat01@gmail.com>
2022-12-06 23:21:38 +0100

    solve part 2 of day 6

--- src/years/2022/days/_6.ts
+++ src/years/2022/days/_6.ts
@@ -3,35 +3,41 @@ import * as path from 'path';
 
 const stringHasRepeats = (toCheck: string): boolean => /(.).*\1/.test(toCheck);
 
-const task1 = (elveDatastreamBuffer: string): void => {
-  const bufferLetters = elveDatastreamBuffer.split('');
+const getMarker = (dataStream: string, sizeOfMarker: number): number => {
+  const bufferLetters = dataStream.split('');
   let processedCharactersIndex = 0;
-  let startOfPacketMarker = '';
+  let startOfXMarker = ''; // if sizeOfMarker is 4 -> X = packet; else X = message
 
   for (
     ;
     processedCharactersIndex < bufferLetters.length;
     processedCharactersIndex++
   ) {
-    if (startOfPacketMarker.length === 4) {
-      startOfPacketMarker = startOfPacketMarker.substring(1);
+    if (startOfXMarker.length === sizeOfMarker) {
+      startOfXMarker = startOfXMarker.substring(1);
     }
 
-    startOfPacketMarker += bufferLetters[processedCharactersIndex];
+    startOfXMarker += bufferLetters[processedCharactersIndex];
 
     if (
-      startOfPacketMarker.length === 4 &&
-      !stringHasRepeats(startOfPacketMarker)
+      startOfXMarker.length === sizeOfMarker &&
+      !stringHasRepeats(startOfXMarker)
     ) {
       ++processedCharactersIndex; // because of 0 index, but we need the sum of processed chars
       break;
     }
   }
 
-  console.log(processedCharactersIndex);
+  return processedCharactersIndex;
 };
 
-const task2 = (elveDatastreamBuffer: string): void => {};
+const task1 = (elveDatastreamBuffer: string): void => {
+  console.log(getMarker(elveDatastreamBuffer, 4));
+};
+
+const task2 = (elveDatastreamBuffer: string): void => {
+  console.log(getMarker(elveDatastreamBuffer, 14));
+};
 
 const main = (): void => {
   const elveDatastreamBuffer = reader(path.join(__dirname, '../input/_6.txt'));
